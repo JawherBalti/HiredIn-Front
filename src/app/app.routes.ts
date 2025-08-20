4fa34d37ea25ca9e223f8687e36957301daa6855
farahbalti <fbalti@welyne.com>
2025-08-20 10:43:58 +0100

    add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,19 +9,20 @@ import { JobOfferDetails } from './components/job-offer-details/job-offer-detail
 import { JobApplications } from './components/job-applications/job-applications';
 import { ApplyToJob } from './components/apply-to-job/apply-to-job';
 import { Notifications } from './components/notifications/notifications';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: LandingPage },
     { path: 'login', component: Login },
     { path: 'register', component: Register }, // Create this later
-    { path: 'job-offers', component: JobOfferList },
-    { path: 'job-offers/create', component: JobOfferForm },
-    { path: 'company/create', component: CompanyForm },
-    { path: 'job-offers/:id/edit', component: JobOfferForm },
-    { path: 'job-offers/:id', component: JobOfferDetails },
-    { path: 'jobs/:id/apply', component: ApplyToJob },
-    { path: 'jobs/:id/applications', component: JobApplications },
-    { path: 'notifications', component: Notifications}
+    { path: 'job-offers', component: JobOfferList, canActivate: [AuthGuard] },
+    { path: 'job-offers/create', component: JobOfferForm, canActivate: [AuthGuard] },
+    { path: 'company/create', component: CompanyForm, canActivate: [AuthGuard] },
+    { path: 'job-offers/:id/edit', component: JobOfferForm, canActivate: [AuthGuard] },
+    { path: 'job-offers/:id', component: JobOfferDetails, canActivate: [AuthGuard] },
+    { path: 'jobs/:id/apply', component: ApplyToJob, canActivate: [AuthGuard] },
+    { path: 'jobs/:id/applications', component: JobApplications, canActivate: [AuthGuard] },
+    { path: 'notifications', component: Notifications, canActivate: [AuthGuard]}
 //   { path: 'apply', component: ApplyComponent, canActivate: [AuthGuard]  }, // Create this later
 //   { 
 //     path: 'dashboard', 
